5ca717b2c23bc32196b855a1833f7aa3b72fed6f
Encep Suryana <encep.suryanajr@gmail.com>
2025-02-23 23:38:47 +0700

    Update footer version text to reflect resume version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: FC = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <h4 className="text-white text-md font-primary">
             &copy; 2021 Encep Suryana. All Rights Reserved. Made with ❤️ in
-            Next.js | v.2.0.0
+            Next.js | Resume v.2.0
           </h4>
         </div>
       </div>
